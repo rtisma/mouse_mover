14b687e01412a3dea99927984895983d04d23020
Robert Tisma <942951+rtisma@users.noreply.github.com>
2019-10-07 09:36:07 -0400

    updated

--- src/main/java/com/roberttisma/tools/mouse_mover/Main.java
+++ src/main/java/com/roberttisma/tools/mouse_mover/Main.java
@@ -8,14 +8,13 @@ public class Main {
     Robot robot = new Robot();
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     int w = (int)screenSize.getWidth()/4;
-    Point current = MouseInfo.getPointerInfo().getLocation();
-    Point initial = current;
+    Point initial = MouseInfo.getPointerInfo().getLocation();
     int xInit = (int)initial.getX();
     int yInit = (int)initial.getY();
     int x = xInit;
     int y = yInit;
     while(true){
-      if (MouseInfo.getPointerInfo().getLocation().getY() ==  current.getY()){
+      if (MouseInfo.getPointerInfo().getLocation().getY() ==  initial.getY()){
         x = x > (xInit + w) ? xInit : x+1;
         robot.mouseMove(x, y);
         Thread.sleep(1);
