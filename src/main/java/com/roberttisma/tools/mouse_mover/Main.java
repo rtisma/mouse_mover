a91ed8c0bf755a315f51db529603ba5e1f1ca93f
Robert Tisma <942951+rtisma@users.noreply.github.com>
2019-10-08 09:56:12 -0400

    updated

--- src/main/java/com/roberttisma/tools/mouse_mover/Main.java
+++ src/main/java/com/roberttisma/tools/mouse_mover/Main.java
@@ -1,27 +1,56 @@
 package com.roberttisma.tools.mouse_mover;
 
 import java.awt.*;
+import java.util.ArrayList;
+
+import static java.lang.Math.cos;
+import static java.lang.Math.floorMod;
+import static java.lang.Math.min;
+import static java.lang.Math.sin;
 
 public class Main {
 
   public static void main(String[] args) throws Throwable{
     Robot robot = new Robot();
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    int w = (int)screenSize.getWidth()/4;
-    Point initial = MouseInfo.getPointerInfo().getLocation();
-    int xInit = (int)initial.getX();
-    int yInit = (int)initial.getY();
-    int x = xInit;
-    int y = yInit;
+    Point centerPoint = new Point((int)screenSize.getWidth()/2, (int)screenSize.getHeight()/2);
+    int radius = (int)min(screenSize.getHeight(), screenSize.getWidth())/4;
+    ArrayList<Point> points = generateCirclePoints3(centerPoint, radius, 0.03);
+
+    Point firstPoint = points.get(0);
+    robot.mouseMove((int)firstPoint.getX(), (int)firstPoint.getY());
+
+    long delay = 5;
+    Point currPoint = MouseInfo.getPointerInfo().getLocation();
+    Point actualPoint = currPoint;
+    int currIdx = 0;
     while(true){
-      if (MouseInfo.getPointerInfo().getLocation().getY() ==  initial.getY()){
-        x = x > (xInit + w) ? xInit : x+1;
-        robot.mouseMove(x, y);
-        Thread.sleep(1);
-      } else {
+      if (!actualPoint.equals(currPoint)){
         break;
       }
+      currIdx= floorMod(currIdx+1, points.size());
+      currPoint = points.get(currIdx);
+
+      robot.mouseMove((int)currPoint.getX(), (int)currPoint.getY());
+      Thread.sleep(delay);
+      actualPoint = MouseInfo.getPointerInfo().getLocation();
     }
+  }
+
+  private static ArrayList<Point> generateCirclePoints3(Point center, int radius, double tx){
+    ArrayList<Point> points = new ArrayList<Point>();
+    double xCenter = center.getX();
+    double yCenter = center.getY();
+    double y;
+    double x;
+    for (double theta=0; theta<2*Math.PI; theta += tx){
+      x = radius* sin(theta);
+      y = radius* cos(theta);
+      Point p = new Point();
+      p.setLocation(x+xCenter, y+yCenter);
+      points.add(p);
+    }
+    return points;
   }
 
 }
